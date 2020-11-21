da67086ec0a375c570aafd91d5a8cdda735726fb
Mandarito <62321617+Mandarito@users.noreply.github.com>
2020-11-20 22:13:56 -0400

    Add files via upload

---
+++ PatronSingleton_CSharp/Singleton.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatronSingleton_CSharp
+{
+    class Singleton
+    {
+        private static Singleton instance = null;
+        public string mensaje = "";
+         
+
+        
+        private Singleton() 
+        {
+            mensaje = "Buenas Profesor Kelyn Tejada";
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        
+    }
+}
